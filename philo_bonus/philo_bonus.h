61a7e89cd52847978a1d488f43d361b725ae1ed3
Walid Oumechtak <woumecht@e1r1p12.1337.ma>
2023-02-13 11:58:59 +0100

    go

---
+++ philo_bonus/philo_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: woumecht <woumecht@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 15:30:35 by woumecht          #+#    #+#             */
+/*   Updated: 2023/02/13 10:43:41 by woumecht         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+#define PHILO_BONUS_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+
+
+#endif
